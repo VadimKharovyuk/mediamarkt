3794c74c3dcd33f83d4a346324b90d4353ee1b53
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-05-22 15:59:06 +0300

    add categories

---
+++ src/main/java/com/example/mediamarkt/controller/CategoryController.java
@@ -0,0 +1,73 @@
+package com.example.mediamarkt.controller;
+import com.example.mediamarkt.model.Category;
+import com.example.mediamarkt.service.CategoryService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/categories")
+@AllArgsConstructor
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @GetMapping
+    public String getAllCategories(Model model) {
+        List<Category> categories = categoryService.getAllCategories();
+        model.addAttribute("categories", categories);
+        return "categories"; // Имя шаблона для отображения всех категорий
+    }
+
+    @GetMapping("/{id}")
+    public String getCategoryById(@PathVariable Long id, Model model) {
+        Optional<Category> category = categoryService.getCategoryById(id);
+        if (category.isPresent()) {
+            model.addAttribute("category", category.get());
+            return "category"; // Имя шаблона для отображения категории по ID
+        } else {
+            return "category-not-found"; // Имя шаблона для отображения ошибки
+        }
+    }
+
+    @GetMapping("/new")
+    public String showCreateCategoryForm(Model model) {
+        model.addAttribute("category", new Category());
+        return "create-category"; // Имя шаблона для создания новой категории
+    }
+
+    @PostMapping
+    public String createCategory(@ModelAttribute Category category) {
+        categoryService.saveCategory(category);
+        return "redirect:/categories";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showEditCategoryForm(@PathVariable Long id, Model model) {
+        Optional<Category> category = categoryService.getCategoryById(id);
+        if (category.isPresent()) {
+            model.addAttribute("category", category.get());
+            return "edit-category"; // Имя шаблона для редактирования категории
+        } else {
+            return "category-not-found"; // Имя шаблона для отображения ошибки
+        }
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateCategory(@PathVariable Long id, @ModelAttribute Category category) {
+        category.setId(id);
+        categoryService.saveCategory(category);
+        return "redirect:/categories";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteCategory(@PathVariable Long id) {
+        categoryService.deleteCategory(id);
+        return "redirect:/categories";
+    }
+}
+
--- src/main/java/com/example/mediamarkt/controller/ProductController.java
+++ src/main/java/com/example/mediamarkt/controller/ProductController.java
@@ -3,13 +3,16 @@ package com.example.mediamarkt.controller;
 import com.example.mediamarkt.model.Product;
 import com.example.mediamarkt.service.ProductService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+
 @Controller
+@RequestMapping("/products")
 @AllArgsConstructor
 public class ProductController {
     private final ProductService productService;
@@ -18,5 +21,36 @@ public class ProductController {
         List<Product> products = productService.searchProductsByName(name);
         model.addAttribute("products", products);
         return "search-results"; // возвращаем имя страницы для отображения результатов
+    }
+    @GetMapping
+    public String getAllProducts(Model model) {
+        List<Product> products = productService.getAllProducts();
+        model.addAttribute("products", products);
+        return "products"; // Имя шаблона для отображения всех продуктов
+    }
+
+    @GetMapping("/{id}")
+    public String getProductById(@PathVariable Long id, Model model) {
+        Optional<Product> product = productService.getProductById(id);
+        if (product.isPresent()) {
+            model.addAttribute("product", product.get());
+            return "product"; // Имя шаблона для отображения продукта по ID
+        } else {
+            return "product-not-found"; // Имя шаблона для отображения ошибки
+        }
+    }
+
+
+
+    @PostMapping
+    public String saveProduct(@ModelAttribute Product product) {
+        productService.saveProduct(product);
+        return "redirect:/products"; // Перенаправление на список продуктов после сохранения
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+        return ResponseEntity.noContent().build();
     }
 }
